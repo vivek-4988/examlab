74284f7b8b454fdce7da58a59485982acb0aa0ac
vivek <vivek@creatingwinners.com>
2023-12-07 14:03:18 +0800

    Project creation JSON load from view model

---
+++ app/src/main/java/com/vivek/pricescreen/model/SpecificationItem.kt
@@ -0,0 +1,11 @@
+package com.vivek.pricescreen.model
+
+data class SpecificationItem(
+    val _id: String,
+    val is_default_selected: Boolean,
+    val name: List<String>,
+    val price: Int,
+    val sequence_number: Int,
+    val specification_group_id: String,
+    val unique_id: Int
+)
